0db2d3575332cb2d4d27927926e892bd0643a54b
kiarash <kiarasha98@gmail.com>
2020-12-18 12:12:29 +0330

    migrated to junit4

--- src/test/java/bdd/BDDEntryPointAcceptanceTest.java
+++ src/test/java/bdd/BDDEntryPointAcceptanceTest.java
@@ -6,12 +6,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	publish = true,
 	glue = "bdd",
 	tags = "not @ignore",
 	monochrome = true,
 	plugin = {"pretty", "summary" ,"html:target/cucumber/cucumber.html", "json:target/cucumber.json"},
-	features = {"src/test/resources/features"})
+	features = {"classpath:features/"})
 public class BDDEntryPointAcceptanceTest {
 
 }
