72acac4f0ff5c98b17121c0214d3f6aab0a154fd
awwmiir <amir.alizad@ut.ac.ir>
2021-10-10 16:43:22 +0330

    Add test for owner's telephone

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerTest.java
@@ -8,7 +8,10 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +19,7 @@ class OwnerTest {
 	private Owner owner;
 	private ValidatorFactory factory;
 	private Validator validator;
+	List<ConstraintViolation<Owner>> constraintViolations;
 
 	@BeforeEach
 	void setUp(){
@@ -24,17 +28,27 @@ class OwnerTest {
 		owner.setLastName("Aliz");
 		owner.setCity("Tehran");
 		owner.setAddress("Rey");
+		constraintViolations = new ArrayList<>();
 		factory = Validation.buildDefaultValidatorFactory();
 		validator = factory.getValidator();
 	}
 
 	@Test
-	public void Owner_can_not_have_empty_fields(){
+	public void Valid_owner_can_not_have_empty_fields(){
 		owner.setCity("");
 		owner.setFirstName("");
 		owner.setAddress("");
 		owner.setLastName("");
-		Set<ConstraintViolation<Owner>> constraintViolations = validator.validate(owner);
+		constraintViolations.addAll(validator.validate(owner));
 		assertEquals(5, constraintViolations.size(), "Owner has 5 empty fields.");
 	}
+
+	@Test
+	public void Owners_telephone_can_not_contain_characters(){
+		owner.setTelephone("123456789a");
+		constraintViolations.addAll(validator.validate(owner));
+		assertEquals(1, constraintViolations.size());
+		assertEquals("numeric value out of bounds (<10 digits>.<0 digits> expected)", constraintViolations.get(0).getMessage());
+	}
+
 }
