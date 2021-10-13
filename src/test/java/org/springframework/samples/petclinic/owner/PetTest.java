d041a419c70b354bb74967a397072f5e7fdc04e8
awwmiir <amir.alizad@ut.ac.ir>
2021-10-13 12:40:03 +0330

    Add setUp methods for PetTest

--- src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -17,12 +17,15 @@ import java.util.*;
 public class PetTest {
 	private Pet pet;
 	private PetType type;
-
+	private Visit visit1, visit2, visit3;
+	private Set<Visit> visits;
 	public PetTest(){}
 
 	@BeforeEach
 	public void setUp(){
+		visits = new HashSet<>();
 		setUpType();
+		setUpVisit();
 		setUpPet();
 	}
 
@@ -36,5 +39,29 @@ public class PetTest {
 		pet.setName("dog1");
 		pet.setBirthDate(LocalDate.parse("2000-01-01"));
 		pet.setType(type);
+		pet.setId(1);
+	}
+
+	void setUpVisit(){
+		visit1 = new Visit();
+		visit1.setId(1);
+		visit1.setDate(LocalDate.parse("2000-01-02"));
+		visit1.setPetId(1);
+		visit1.setDescription("description for visit1.");
+		visits.add(visit1);
+
+		visit2 = new Visit();
+		visit2.setId(2);
+		visit2.setDate(LocalDate.parse("2000-01-03"));
+		visit2.setPetId(1);
+		visit2.setDescription("description for visit2.");
+		visits.add(visit2);
+
+		visit3 = new Visit();
+		visit3.setId(3);
+		visit3.setDate(LocalDate.parse("2000-01-04"));
+		visit3.setPetId(1);
+		visit3.setDescription("description for visit3.");
+		visits.add(visit3);
 	}
 }
