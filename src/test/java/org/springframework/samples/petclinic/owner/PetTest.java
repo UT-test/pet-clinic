25b073f9453b84791a452d40fb611965cced7f71
awwmiir <amir.alizad@ut.ac.ir>
2021-10-13 13:28:15 +0330

    Add theory test for getVisits method in Pet

--- src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.Theories;
@@ -17,17 +16,8 @@ import java.util.*;
 public class PetTest {
 	private Pet pet;
 	private PetType type;
-	private Visit visit1, visit2, visit3;
-	private Set<Visit> visits;
-	public PetTest(){}
 
-	@BeforeEach
-	public void setUp(){
-		visits = new HashSet<>();
-		setUpType();
-		setUpVisit();
-		setUpPet();
-	}
+	public PetTest(){}
 
 	void setUpType(){
 		type = new PetType();
@@ -42,26 +32,77 @@ public class PetTest {
 		pet.setId(1);
 	}
 
-	void setUpVisit(){
+	@DataPoint
+	public static List<Visit> visits1() {
+		List<Visit> visits = new ArrayList<>();
+		Visit visit1, visit2, visit3;
+
 		visit1 = new Visit();
 		visit1.setId(1);
-		visit1.setDate(LocalDate.parse("2000-01-02"));
+		visit1.setDate(LocalDate.parse("2000-01-05"));
 		visit1.setPetId(1);
-		visit1.setDescription("description for visit1.");
+		visit1.setDescription("visit1.");
 		visits.add(visit1);
 
 		visit2 = new Visit();
 		visit2.setId(2);
-		visit2.setDate(LocalDate.parse("2000-01-03"));
+		visit2.setDate(LocalDate.parse("2000-01-06"));
 		visit2.setPetId(1);
-		visit2.setDescription("description for visit2.");
+		visit2.setDescription("visit2.");
 		visits.add(visit2);
 
 		visit3 = new Visit();
 		visit3.setId(3);
-		visit3.setDate(LocalDate.parse("2000-01-04"));
+		visit3.setDate(LocalDate.parse("2000-01-03"));
 		visit3.setPetId(1);
-		visit3.setDescription("description for visit3.");
+		visit3.setDescription("visit3.");
 		visits.add(visit3);
+
+		return visits;
+	}
+
+	@DataPoint
+	public static List<Visit> visits2() {
+		List<Visit> visits = new ArrayList<>();
+		Visit visit1, visit2, visit3;
+
+		visit1 = new Visit();
+		visit1.setId(1);
+		visit1.setDate(LocalDate.parse("2000-03-05"));
+		visit1.setPetId(1);
+		visit1.setDescription("visit1.");
+		visits.add(visit1);
+
+		visit2 = new Visit();
+		visit2.setId(2);
+		visit2.setDate(LocalDate.parse("2000-02-06"));
+		visit2.setPetId(1);
+		visit2.setDescription("visit2.");
+		visits.add(visit2);
+
+		visit3 = new Visit();
+		visit3.setId(3);
+		visit3.setDate(LocalDate.parse("2000-05-03"));
+		visit3.setPetId(1);
+		visit3.setDescription("visit3.");
+		visits.add(visit3);
+
+		return visits;
+	}
+
+	@Theory
+	public void visits_are_sorted_by_their_date(List<Visit> expected){
+		System.out.println("--------------------------------------------------------------");
+		setUpType();
+		setUpPet();
+		expected.forEach(visit -> pet.addVisit(visit));
+		expected.sort((o1, o2) -> o2.getDate().compareTo(o1.getDate()));
+		System.out.println("Expected:");
+		expected.forEach(visit -> System.out.println(visit.getDescription() + ":" + visit.getDate().toString()));
+		List<Visit> actual = pet.getVisits();
+		assertEquals(expected, actual);
+		System.out.println("Actual:");
+		actual.forEach(visit -> System.out.println(visit.getDescription() + ":" + visit.getDate().toString()));
+
 	}
 }
