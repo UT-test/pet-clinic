8402d861b94b4ca09c009f7309d3c31f90ea469b
awwmiir <amir.alizad@ut.ac.ir>
2021-10-13 12:33:31 +0330

    Add setUp methods for PetTest

--- src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTest.java
@@ -1,11 +1,40 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 import org.springframework.samples.petclinic.visit.Visit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.time.LocalDate;
+import java.util.*;
 
-class PetTest {
 
-	// todo
+@RunWith(Theories.class)
+public class PetTest {
+	private Pet pet;
+	private PetType type;
+
+	public PetTest(){}
+
+	@BeforeEach
+	public void setUp(){
+		setUpType();
+		setUpPet();
+	}
+
+	void setUpType(){
+		type = new PetType();
+		type.setName("dog");
+	}
+
+	void setUpPet(){
+		pet = new Pet();
+		pet.setName("dog1");
+		pet.setBirthDate(LocalDate.parse("2000-01-01"));
+		pet.setType(type);
+	}
 }
