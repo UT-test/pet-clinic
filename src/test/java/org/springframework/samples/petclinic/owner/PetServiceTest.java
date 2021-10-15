322976f8e6a92367f2d19234f9f4464da4b608d7
awwmiir <amir.alizad@ut.ac.ir>
2021-10-15 16:03:28 +0330

    Add assumptions to parametrized test

--- src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Arrays;
 import java.util.Collection;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.*;
@@ -56,12 +57,17 @@ public class PetServiceTest {
 		return Arrays.asList(new Object[][]{
 			{1, pet1},
 			{2, pet2},
-			{3, pet3}
+			{3, pet3},
+			{-1, pet2},
+			{0, pet1},
+			{1, null}
 		});
 	}
 
 	@Test
-	public void Pets_are_found_correctly() {
+	public void Pets_are_found_correctly(){
+		assumeTrue(this.expectedId >= 1);
+		assumeNotNull(this.expectedPet);
 		setUp();
 		System.out.println("------------------------------------------------------------");
 		System.out.printf("parametrized test with id: %d\n", expectedId);
