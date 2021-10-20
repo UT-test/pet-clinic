8bf762f7a12b1d44e79bc43987ebc661b630c057
Bardia Eghbali <bardia.eghbali@gmail.com>
2021-10-20 17:03:40 +0330

    Clean up SimplePriceCalculator

--- src/main/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculator.java
+++ src/main/java/org/springframework/samples/petclinic/model/priceCalculators/SimplePriceCalculator.java
@@ -1,7 +1,5 @@
 package org.springframework.samples.petclinic.model.priceCalculators;
 
-import org.joda.time.DateTime;
-import org.joda.time.Years;
 import org.springframework.samples.petclinic.model.Pet;
 import org.springframework.samples.petclinic.model.UserType;
 
@@ -9,11 +7,7 @@ import java.util.List;
 
 public class SimplePriceCalculator implements PriceCalculator {
 
-    private static int INFANT_YEARS = 2;
-    private static double RARE_INFANCY_COEF = 1.4;
-    private static double COMMON_INFANCY_COEF = 1.2;
-    private static double BASE_RARE_COEF = 1.2;
-    private static int DISCOUNT_MIN_SCORE = 10;
+    private static final double BASE_RARE_COEF = 1.2;
 
     @Override
     public double calcPrice(List<Pet> pets, double baseCharge, double basePricePerPet, UserType userType) {
