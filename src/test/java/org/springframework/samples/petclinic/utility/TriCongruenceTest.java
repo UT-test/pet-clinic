1f13f2b983fdb6b10856bf0f107df492aa59dec3
TheRealRondon <amirhossein2324236@gmail.com>
2021-11-26 19:13:55 +0330

    Added clause coverage tests.

--- src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/TriCongruenceTest.java
@@ -9,6 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ExtendWith(ReportingExtension.class)
+
+@ClauseDefinition(clause = 'a', def = "t1arr[0]")
+@ClauseDefinition(clause = 'b', def = "t1arr[0] + t1arr[1] < t1arr[2]")
+
 class TriCongruenceTest {
 
 	private static final Logger log = LoggerFactory.getLogger(TriCongruenceTest.class);
@@ -23,9 +27,49 @@ class TriCongruenceTest {
 	}
 
 	/**
-	 * TODO
-	 * explain your answer here
+	 For clause coverage we have or predicate of a and b clauses
+	 As we are using clause coverage we should check all values for both clauses
+	 which includes a = True and a = False
+	 and b = True and b = False
+	 We make two tests:
+	 1) a = true and b = true
+	 2) a = false and b = false
 	 */
+
+	@ClauseCoverage(
+		predicate = "a + b",
+		valuations = {
+			@Valuation(clause = 'a', valuation = false),
+			@Valuation(clause = 'b', valuation = false)
+		}
+	)
+	@Test
+	public void Line15ClauseCoverageTest1() {
+		Triangle t1 = new Triangle(2, 2, 3);
+		Triangle t2 = new Triangle(2, 2, 3);
+
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+
+		Assertions.assertTrue(areCongruent);
+	}
+
+	@ClauseCoverage(
+		predicate = "a + b",
+		valuations = {
+			@Valuation(clause = 'a', valuation = true),
+			@Valuation(clause = 'b', valuation = true)
+		}
+	)
+	@Test
+	public void Line15ClauseCoverageTest2() {
+		Triangle t1 = new Triangle(-1, 2, 3);
+		Triangle t2 = new Triangle(-1, 2, 3);
+
+		boolean areCongruent = TriCongruence.areCongruent(t1, t2);
+
+		Assertions.assertFalse(areCongruent);
+	}
+
 	private static boolean questionTwo(boolean a, boolean b, boolean c, boolean d, boolean e) {
 		boolean predicate = false;
 //		predicate = a predicate with any number of clauses
